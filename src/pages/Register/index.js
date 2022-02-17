b45ace3f5ddc255bc877a484296b968bba018d69
Daniela Farias <farias.danicris@gmail.com>
2022-02-17 03:09:36 -0300

    Cadastro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import {
-  InputLabel,
   MenuItem,
   Select,
   TextField,
@@ -10,29 +9,49 @@ import {
 } from "@mui/material";
 import { Form } from "./style";
 import { postUser } from "../../api";
+import moment from "moment";
 
 export default function Register() {
- const [data, setData] = useState({
-     firstName: '',
-     lastName: '',
-     birthdate: '',
-     occupation: '',
-     email: '',
-     password: '',
-     passwordConfirmation: '',
-     imageUrl: '',
-     role: 'USER'
- });
+  const now = moment(new Date()).format("yyyy-MM-dd");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthdate, setBirthdate] = useState(now);
+  const [occupation, setOccupation] = useState("Cliente");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [role, setRole] = useState("USER");
 
-  const handleSubmit = () => {
+  useEffect(() => {
+    if (occupation === "Cliente") {
+      setRole('USER');
+    } else {
+      setRole('ADMIN');
+    }
+  }, [occupation]);
 
-      
-      
-};
-console.log(data)
+  const data = {
+    firstName,
+    lastName,
+    birthdate: birthdate + 'T00:00:00.000Z',
+    occupation,
+    email,
+    password,
+    passwordConfirmation,
+    imageUrl,
+    role
+  };
+
+  const handleSubmit = async () => {
+    console.log(data);
+    console.log(birthdate + 'T00:00:00.000Z');
+    const response = await postUser(data);
+    console.log(response);
+  };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form>
       <Grid
         container
         display="flex"
@@ -56,8 +75,8 @@ console.log(data)
             <TextField
               label="Nome"
               variant="filled"
-              defaultValue={data.firstName}
-              onChange={(e) => data.firstName = e.targetValue && setData(data)}
+              defaultValue={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
               name="firstName"
               type="text"
             />
@@ -67,6 +86,8 @@ console.log(data)
               label="Sobrenome"
               variant="filled"
               name="lastName"
+              defaultValue={lastName}
+              onChange={(e) => setLastName(e.target.value)}
               type="text"
             />
           </Grid>
@@ -86,6 +107,8 @@ console.log(data)
               variant="filled"
               name="email"
               type="email"
+              defaultValue={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </Grid>
           <Grid item>
@@ -94,6 +117,8 @@ console.log(data)
               variant="filled"
               name="password"
               type="password"
+              defaultValue={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </Grid>
         </Grid>
@@ -111,6 +136,8 @@ console.log(data)
               variant="filled"
               name="passwordConfirmation"
               type="password"
+              defaultValue={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
             />
           </Grid>
           <Grid item>
@@ -119,43 +146,45 @@ console.log(data)
               variant="filled"
               name="imageUrl"
               type="text"
+              defaultValue={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
             />
           </Grid>
         </Grid>
-        <Grid
-          container
-          display="flex"
-          flexWrap="wrap"
-          alignItems="center"
-          justifyContent="center"
-          spacing={2}
-        >
-          <Grid item>
-            <TextField
-              label="Data de nascimento"
-              variant="filled"
-              name="birthdate"
-              type="date"
-              InputLabelProps={{ shrink: true }}
-            />
-          </Grid>
-          <Grid item>
-            <Select variant="filled" name="occupation" defaultValue="Cliente">
-              <MenuItem value="Cliente">Cliente</MenuItem>
-              <MenuItem value="Garçom">Garçom</MenuItem>
-              <MenuItem value="Chefe">Chefe</MenuItem>
-              <MenuItem value="Cozinheiro">Cozinheiro</MenuItem>
-              <MenuItem value="Auxiliar">Auxiliar de Cozinha</MenuItem>
-              <MenuItem value="Bartender">Bartender</MenuItem>
-              <MenuItem value="Zelador">Zelador</MenuItem>
-              <MenuItem value="Segurança">Segurança</MenuItem>
-            </Select>
-          </Grid>
+
+        <Grid item>
+          <TextField
+            label="Data de nascimento"
+            variant="filled"
+            name="birthdate"
+            type="date"
+            InputLabelProps={{ shrink: true }}
+            defaultValue={birthdate}
+            onChange={(e) => setBirthdate(e.target.value)}
+          />
         </Grid>
         <Grid item>
-            <Button type="submit" variant="contained">
-                Confirmar
-            </Button>
+          <Select
+            variant="filled"
+            name="occupation"
+            defaultValue={occupation}
+            onChange={(e) => setOccupation(e.target.value)}
+          >
+            <MenuItem value="Cliente">Cliente</MenuItem>
+            <MenuItem value="Garçom">Garçom</MenuItem>
+            <MenuItem value="Chefe">Chefe</MenuItem>
+            <MenuItem value="Cozinheiro">Cozinheiro</MenuItem>
+            <MenuItem value="Auxiliar">Auxiliar de Cozinha</MenuItem>
+            <MenuItem value="Bartender">Bartender</MenuItem>
+            <MenuItem value="Zelador">Zelador</MenuItem>
+            <MenuItem value="Segurança">Segurança</MenuItem>
+          </Select>
+        </Grid>
+
+        <Grid item>
+          <Button variant="contained" onClick={() => handleSubmit()}>
+            Confirmar
+          </Button>
         </Grid>
       </Grid>
     </Form>
--- src/pages/Register/style.js
+++ src/pages/Register/style.js
@@ -1,6 +1,17 @@
 import styled from "styled-components";
 
 export const Form = styled.form`
-  
+  h4 {
+    margin: 1rem 0;
+    color: #fcd83c;
+    font-weight: bold;
+  }
 
+  .MuiFormControl-root {
+    margin-bottom: 1rem;
+  }
+
+  .MuiButton-root {
+    background-color: #252f49;
+  }
 `;
